b2fce7ddf8ee9c6e5bb0fed13f4cf6908b64457f
Raimon Esteve <raimon@nan-tic.com>
2017-09-07 15:08:31 +0200

    FIX import classes Since changeset-20f62d472bf9

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
-from . shipment import shipment
+from . import shipment
 
 def register():
     Pool.register(
