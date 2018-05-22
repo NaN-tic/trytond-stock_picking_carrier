4cd96e2c1556ffc1e41663d7b06a3400c438ae86
Raimon Esteve <raimon@nan-tic.com>
2018-05-22 11:30:41 +0200

    Stock Shipment Out Scanning with carrier and labels task-030266

--- shipment.py
+++ shipment.py
@@ -7,7 +7,8 @@ from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 
-__all__ = ['ShipmentOutPicking', 'ShipmentOutPickingResult', 'ShipmentOutPacked']
+__all__ = ['ShipmentOutPicking', 'ShipmentOutPickingResult', 'ShipmentOutPacked',
+    'ShipmentOutScanningStart', 'ShipmentOutScanningResult', 'ShipmentOutScanning']
 
 
 class ShipmentOutPicking:
@@ -40,8 +41,7 @@ class ShipmentOutPacked:
             })
 
     def transition_packed(self):
-        pool = Pool()
-        Shipment = pool.get('stock.shipment.out')
+        Shipment = Pool().get('stock.shipment.out')
 
         super(ShipmentOutPacked, self).transition_packed()
         shipment = self.result.shipment
@@ -82,6 +82,69 @@ class ShipmentOutPacked:
 
     def default_result(self, fields):
         res = super(ShipmentOutPacked, self).default_result(fields)
+        res['label'] = self.result.label
+        res['label_name'] =self.result.label_name
+        return res
+
+
+class ShipmentOutScanningStart:
+    __metaclass__ = PoolMeta
+    __name__ = 'stock.shipment.out.scanning.start'
+    carrier = fields.Boolean('Carrier',
+        help='Send shipment to API carrier')
+
+    @staticmethod
+    def default_carrier():
+        return True
+
+
+class ShipmentOutScanningResult:
+    __metaclass__ = PoolMeta
+    __name__ = 'stock.shipment.out.scanning.result'
+    label = fields.Binary('Label', filename='label_name')
+    label_name = fields.Char('Label Name')
+
+
+class ShipmentOutScanning:
+    __metaclass__ = PoolMeta
+    __name__ = 'stock.shipment.out.scanning'
+
+    @classmethod
+    def __setup__(cls):
+        super(ShipmentOutScanning, cls).__setup__()
+        cls._error_messages.update({
+            'not_label': 'Not return API "%(carrier)s" a label',
+            })
+
+    def transition_packed(self):
+        Shipment = Pool().get('stock.shipment.out')
+
+        packed = super(ShipmentOutScanning, self).transition_packed()
+        if packed == 'start':
+            return 'start'
+        shipment = self.result.shipment
+
+        # Send shipment to carrier API
+        if self.start.carrier and shipment.carrier:
+            refs, labs, errs = Shipment.send_shipment_api(shipment)
+
+            if labs:
+                lab, = labs
+                self.result.labs = labs
+                self.result.label = fields.Binary.cast(open(lab, "rb").read())
+                self.result.label_name = lab.split('/')[2]
+                return 'result'
+            else:
+                self.result.note += self.raise_user_error('not_label', {
+                        'carrier': shipment.carrier.rec_name,
+                        }, raise_exception=False)
+        self.result.labs = None
+        self.result.label = None
+        self.result.label_name = None
+        return 'result'
+
+    def default_result(self, fields):
+        res = super(ShipmentOutScanning, self).default_result(fields)
         res['label'] = self.result.label
         res['label_name'] =self.result.label_name
         return res
